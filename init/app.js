ce88d2a51dc1dcbdb969c75a1c7621b7860c4ada
Shubham Singh <singhofficial1611@gmail.com>
2024-11-18 23:13:47 +0530

    changing url

--- init/app.js
+++ init/app.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect(process.env.CONNECT_URL);
 } 
 main()
       .then(()=>{console.log("connection secure");})
